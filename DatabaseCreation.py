d2f6f38ba933eeed16f37ddab0885ae9b9090b3e
maroman <maroman@umich.edu>
2023-12-01 14:45:55 -0500

    Newest

--- DatabaseCreation.py
+++ DatabaseCreation.py
@@ -15,19 +15,19 @@ def set_up_database(db_name):
 # Creates tables with all-star player stats from each year in [2000, 2005, 2010, 2015, 2020]
 def create_all_stars_tables(cur, conn):
     # Creates 2000 player table with pts, assists, position
-    cur.execute('CREATE TABLE IF NOT EXISTS 2000_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC)')
+    cur.execute('CREATE TABLE IF NOT EXISTS 2000_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC games_played NUMERIC)')
 
     # Creates 2005 player table with pts, assists, position
-    cur.execute('CREATE TABLE IF NOT EXISTS 2005_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC)')
+    cur.execute('CREATE TABLE IF NOT EXISTS 2005_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC games_played NUMERIC)')
 
     # Creates 2010 player table with pts, assists, position
-    cur.execute('CREATE TABLE IF NOT EXISTS 2010_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC)')
+    cur.execute('CREATE TABLE IF NOT EXISTS 2010_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC games_played NUMERIC)')
 
     # Creates 2015 player table with pts, assists, position
-    cur.execute('CREATE TABLE IF NOT EXISTS 2015_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC)')
+    cur.execute('CREATE TABLE IF NOT EXISTS 2015_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC games_played NUMERIC)')
 
     # Creates 2020 player table with pts, assists, position
-    cur.execute('CREATE TABLE IF NOT EXISTS 2020_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC)')
+    cur.execute('CREATE TABLE IF NOT EXISTS 2020_Info (id INTEGER PRIMARY KEY name TEXT position TEXT pts NUMERIC ast NUMERIC games_played NUMERIC)')
     
     conn.commit()
 
@@ -42,13 +42,37 @@ def id_for_player(year_dict):
 
     return player_id_dict
 
+def position_for_player(player_id_dict):
+    player_position_dict = {}
+    for player in player_id_dict:
+        r = requests.get('https://www.balldontlie.io/api/v1/players/' + str(player_id_dict[player]))
+        player_data = r.json()
+        position = player_data['data'][0]['position']
+        player_position_dict[player] = position
+
+    return player_position_dict
+
 def get_player_stats(year_dict, player_id_dict):
     player_stats_by_year = {}
     for year in year_dict:
+        dict_of_player_stats = {}
         for player in year:
             params = {'season': year, 'player_ids': [player_id_dict[player]]}
             r = requests.get('https://www.balldontlie.io/api/v1/season_averages', params=params)
-    
+            raw_data = r.json()
+            pts = float(raw_data['data'][0]['pts'])
+            ast = float(raw_data['data'][0]['ast'])
+            games_played = int(raw_data['data'][0]['games_played'])
+            player_stats = [pts, ast, games_played]
+            dict_of_player_stats[player] = player_stats
+        player_stats_by_year[year] = dict_of_player_stats
 
-def insertData(cur, conn, year_dict):
-    pass
+
+def insertData(cur, conn, player_stats_by_year, player_id_dict, player_position_dict):
+    for year in player_stats_by_year:
+        for player in year:
+            insertion_tuple = (str(year) + '_Info', player_id_dict[player], player_position_dict[player], player_stats_by_year[year][player][0], player_stats_by_year[year][player][1], player_stats_by_year[year][player][2])
+            cur.execute('INSERT OR IGNORE INTO ? (id, name, position, pts, ast, games_played) VALUES (?, ?, ?, ?, ?, ?)', (insertion_tuple))
+
+    conn.commit()
+    # Newest
